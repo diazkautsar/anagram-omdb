ce4dd7fe84f2541660e4ad2a4750e48bccc40e98
Diaz <diazkautsar77@gmail.com>
2021-05-08 12:00:30 +0700

    configure state, setupa index.home

--- omdb/pages/index.tsx
+++ omdb/pages/index.tsx
@@ -1,78 +1,124 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useSelector, connect, useDispatch } from 'react-redux'
-import { wrapper } from '../store'
+import axios from 'axios'
+import React, { ReactNode, useEffect } from 'react'
+import { connect } from 'react-redux'
 import * as types from '../store/types'
 import { State } from '../store/reducer'
+import { faVideo, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Home() {
-  const dispatch = useDispatch()
-  const message = useSelector<State>(state => state.message)
+interface StateProps {
+  message: string
+  list: types.list[]
+}
+
+interface DispatchProps {
+  updateMessage: (message: string) => void
+  updateList: (param: types.list[]) => void
+}
+
+
+type Props = StateProps & DispatchProps
+
+const mapState = (state: State) => ({
+  message: state.message,
+  list: state.list,
+})
+
+const mapDispatch = {
+  updateMessage: (message: string) => ({ type: types.UPDATE_MESSAGE, payload: message }),
+  updateList: (param: types.list[]) => ({ type: types.UPDATE_LIST, payload: param })
+}
+
+function Home({ message, list, updateMessage, updateList }: Props) {
+  const [searchTypeSelected, setSearchTypeSelected] = React.useState<'movie' | 'series' | 'episode' | null>(null)
+  const [querySearch, setQuerySearch] = React.useState<string>('')
+
+  const changeSearchType = (event: React.MouseEvent, param: 'movie' | 'series' | 'episode') => {
+    event.preventDefault()
+    setSearchTypeSelected(param)
+  }
+
+  const searchMovie = async (event: React.MouseEvent) => {
+    event.preventDefault()
+    await axios.get(`/api/fetchmovie?search=${querySearch}&type=${searchTypeSelected}`)
+      .then(({ data }) => {
+        const { data: response } = data
+        updateList(response.Search)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className={styles.selectedButton}>
+        <div
+          style={{ backgroundColor: 'red' }}
+          onClick={(e) => changeSearchType(e, 'movie')}
+          className={styles.buttonMovie}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          Movie
+        </div>
+        <div
+          style={{
+            backgroundColor: 'blue'
+          }}
+          onClick={(e) => changeSearchType(e, 'series')}
+          className="series"
+        >
+          Series
+        </div>
+        <div
+          style={{ backgroundColor: 'green' }}
+          onClick={(e) => changeSearchType(e, 'episode')}
+          className="episode"
+        >
+          Episode
+        </div>
+      </div>
+      <div className={styles.searchBar}>
+        <input type="name" value={querySearch} onChange={(e) => setQuerySearch(e.target.value)} />
+        <button onClick={(e) => searchMovie(e)}> Search </button>
+      </div>
+      <div className={styles.list}>
+        {list.map((item) => (
+          <div key={item.imdbID}>
+            <img style={{ width: '200px' }} src={item.Poster} />
+            <div
+              style={{
+                maxWidth: '200px',
+                textAlign: 'center',
+              }}
+            >
+              {item.Title}
+            </div>
+            <div
+              style={{
+                marginTop: '.75rem',
+                display: 'flex',
+                justifyContent: 'space-between'
+              }}
+            >
+              <FontAwesomeIcon icon={faVideo} />
+              <div> { item.Type } </div>
+            </div>
+            <div
+              style={{
+                marginTop: '.75rem',
+                display: 'flex',
+                justifyContent: 'space-between'
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+              <div> { item.Year } </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps(({ store }) => {
-  store.dispatch({ type: types.UPDATE_MESSAGE, payload: 'Dispatch state' })
-})
-
-export default connect(((state: State) => state))(Home)
+export default connect<StateProps, DispatchProps>(mapState, mapDispatch)(Home)
