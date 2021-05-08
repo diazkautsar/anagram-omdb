c07517d988b2ee520648a755af2e85a4a028c6f6
Diaz <diazkautsar77@gmail.com>
2021-05-08 12:01:27 +0700

    add types

--- omdb/store/types.tsx
+++ omdb/store/types.tsx
@@ -1,1 +1,10 @@
 export const UPDATE_MESSAGE = 'UPDATE_MESSAGE'
+export const UPDATE_LIST = 'UPDATE_LIST'
+
+export type list = {
+  Poster?: string
+  Title?: string
+  Type?: string
+  Year: string
+  imdbID?: string
+}
